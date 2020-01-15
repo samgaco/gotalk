3ae7021a8a956629725a9a1079839859c5d046c0
samuel <samuelgarciacompanys@gmail.com>
2020-01-15 18:18:05 +0100

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-40.times do 
+30.times do 
     Teacher.create(
         name: Faker::Name.first_name,
         language: Faker::Nation.language,
