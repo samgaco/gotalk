0391b2e1470a3d95b5d03e3bd72354afeb345f7a
samuel <samuelgarciacompanys@gmail.com>
2020-01-16 18:18:19 +0100

    new changes

--- app/javascript/components/TeacherCard.jsx
+++ app/javascript/components/TeacherCard.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import './styles/teachers.scss';
+import '../../assets/stylesheets/main.scss';
 
 
 class TeacherCard extends React.Component {
