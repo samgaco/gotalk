baaa349e837aae5a2fd9d3b640f18402274343be
samuel <samuelgarciacompanys@gmail.com>
2020-01-16 11:40:40 +0100

    Linter fixes

---
+++ app/javascript/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div>This is the contact page.</div>
+    );
+  }
+}
+
+export default Contact;
