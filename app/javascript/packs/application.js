5ddb06f917fa4d7bd4162608a08972ae9c741eb2
samuel <samuelgarciacompanys@gmail.com>
2020-01-17 11:10:11 +0100

    new changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,15 @@
 require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
+require('channels');
 import 'bootstrap'
-import './src/application.scss'
-// require('channels');
+import '../stylesheets/application'
 
+document.addEventListener("turbolinks:load", ()=>{
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,6 @@
 const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
-
-environment.loaders.prepend('typescript', typescript)
 const webpack = require('webpack')
+
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
