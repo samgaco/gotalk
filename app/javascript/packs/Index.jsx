0b655144b36654f8611ff5b05c49b3bf9488a9e3
samuel <samuelgarciacompanys@gmail.com>
2020-01-17 10:40:12 +0100

    new changes

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { createStore} from 'redux';
 import { Provider } from 'react-redux';
 import App from '../components/App';
