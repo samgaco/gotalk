ac7fef48ee9f032e375642e94150821401ddd965
samuel <samuelgarciacompanys@gmail.com>
2020-01-23 22:52:15 +0100

    Fix length

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 # Model for likes
-
 class Like < ApplicationRecord
   belongs_to :teacher
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@
 
 # Include default devise modules. Others available are:
 # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
